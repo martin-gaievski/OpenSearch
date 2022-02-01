be8f2ec8c583762ba6c6c8c73a3dbfeea2a9005e
John Mazanec <jmazane@amazon.com>
2022-02-01 12:12:55 -0800

    Port plugin knn mapper and query to OpenSearch
    Copies and integrates the k-NN field mapper and query builder into
    OpenSearch to use Lucene's ANN functionality. Currently, it does not set
    any parameters for Lucene's HNSW functionality. It works in more or less
    the same way that the plugin does from an interface perspective.
    
    Signed-off-by: John Mazanec <jmazane@amazon.com>

---
+++ server/src/main/java/org/opensearch/index/mapper/KNNVectorFieldMapper.java
@@ -0,0 +1,267 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.index.mapper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.KnnVectorField;
+import org.apache.lucene.index.DocValuesType;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.VectorSimilarityFunction;
+import org.apache.lucene.search.DocValuesFieldExistsQuery;
+import org.apache.lucene.search.Query;
+import org.opensearch.common.Explicit;
+import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.common.xcontent.support.XContentMapValues;
+import org.opensearch.index.query.QueryShardContext;
+import org.opensearch.index.query.QueryShardException;
+import org.opensearch.search.lookup.SearchLookup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Field Mapper for KNN vector type.
+ *
+ * Extends ParametrizedFieldMapper in order to easily configure mapping parameters.
+ */
+public class KNNVectorFieldMapper extends ParametrizedFieldMapper {
+
+    private static final Logger logger = LogManager.getLogger(KNNVectorFieldMapper.class);
+
+    public static final String CONTENT_TYPE = "knn_vector";
+
+    /**
+     * Define the max dimension a knn_vector mapping can have. This limit is somewhat arbitrary. In the future, we
+     * should make this configurable.
+     */
+    public static final int MAX_DIMENSION = 10000;
+
+    private static KNNVectorFieldMapper toType(FieldMapper in) {
+        return (KNNVectorFieldMapper) in;
+    }
+
+    /**
+     * Builder for KNNVectorFieldMapper. This class defines the set of parameters that can be applied to the knn_vector
+     * field type
+     */
+    public static class Builder extends ParametrizedFieldMapper.Builder {
+
+        protected final Parameter<Integer> dimension = new Parameter<>("dimension", false,
+            () -> -1,
+            (n, c, o) -> {
+                if (o == null) {
+                    throw new IllegalArgumentException("Dimension cannot be null");
+                }
+                int value = XContentMapValues.nodeIntegerValue(o);
+                if (value > MAX_DIMENSION) {
+                    throw new IllegalArgumentException("Dimension value cannot be greater than " +
+                        MAX_DIMENSION + " for vector: " + name);
+                }
+
+                if (value <= 0) {
+                    throw new IllegalArgumentException("Dimension value must be greater than 0 " +
+                        "for vector: " + name);
+                }
+                return value;
+            }, m -> toType(m).dimension);
+
+        public Builder(String name) {
+            super(name);
+        }
+
+        @Override
+        protected List<Parameter<?>> getParameters() {
+            return List.of(dimension);
+        }
+
+        @Override
+        public KNNVectorFieldMapper build(BuilderContext context) {
+            return new KNNVectorFieldMapper(
+                buildFullName(context),
+                new KNNVectorFieldType(buildFullName(context), Collections.emptyMap(), dimension.get()),
+                multiFieldsBuilder.build(this, context),
+                copyTo.build()
+            );
+        }
+    }
+
+    public static class TypeParser implements Mapper.TypeParser {
+
+        @Override
+        public Mapper.Builder<?> parse(String name, Map<String, Object> node, ParserContext parserContext)
+            throws MapperParsingException {
+            Builder builder = new KNNVectorFieldMapper.Builder(name);
+            builder.parse(name, parserContext, node);
+            return builder;
+        }
+    }
+
+    public static class KNNVectorFieldType extends MappedFieldType {
+
+        int dimension;
+
+        public KNNVectorFieldType(String name, Map<String, String> meta, int dimension) {
+            super(name, false, false, true, TextSearchInfo.NONE, meta);
+            this.dimension = dimension;
+        }
+
+        @Override
+        public ValueFetcher valueFetcher(MapperService mapperService, SearchLookup searchLookup, String format) {
+            throw new UnsupportedOperationException("KNN Vector do not support fields search");
+        }
+
+        @Override
+        public String typeName() {
+            return CONTENT_TYPE;
+        }
+
+        @Override
+        public Query existsQuery(QueryShardContext context) {
+            return new DocValuesFieldExistsQuery(name());
+        }
+
+        @Override
+        public Query termQuery(Object value, QueryShardContext context) {
+            throw new QueryShardException(context, "KNN vector do not support exact searching, use KNN queries " +
+                "instead: [" + name() + "]");
+        }
+
+        public int getDimension() {
+            return dimension;
+        }
+    }
+
+    protected Explicit<Boolean> ignoreMalformed;
+    protected boolean stored;
+    protected boolean hasDocValues;
+    protected Integer dimension;
+    protected String modelId;
+
+    public KNNVectorFieldMapper(String simpleName, KNNVectorFieldType mappedFieldType, MultiFields multiFields,
+                                CopyTo copyTo) {
+        super(simpleName, mappedFieldType,  multiFields, copyTo);
+        dimension = mappedFieldType.dimension;
+        fieldType = new FieldType(KNNVectorFieldMapper.Defaults.FIELD_TYPE);
+        fieldType.setVectorDimensionsAndSimilarityFunction(mappedFieldType.getDimension(),
+            VectorSimilarityFunction.EUCLIDEAN);
+        fieldType.freeze();
+    }
+
+    @Override
+    protected String contentType() {
+        return CONTENT_TYPE;
+    }
+
+    @Override
+    protected void parseCreateField(ParseContext context) throws IOException {
+        parseCreateField(context, fieldType().getDimension());
+    }
+
+    protected void parseCreateField(ParseContext context, int dimension) throws IOException {
+        context.path().add(simpleName());
+
+        ArrayList<Float> vector = new ArrayList<>();
+        XContentParser.Token token = context.parser().currentToken();
+        float value;
+        if (token == XContentParser.Token.START_ARRAY) {
+            token = context.parser().nextToken();
+            while (token != XContentParser.Token.END_ARRAY) {
+                value = context.parser().floatValue();
+
+                if (Float.isNaN(value)) {
+                    throw new IllegalArgumentException("KNN vector values cannot be NaN");
+                }
+
+                if (Float.isInfinite(value)) {
+                    throw new IllegalArgumentException("KNN vector values cannot be infinity");
+                }
+
+                vector.add(value);
+                token = context.parser().nextToken();
+            }
+        } else if (token == XContentParser.Token.VALUE_NUMBER) {
+            value = context.parser().floatValue();
+
+            if (Float.isNaN(value)) {
+                throw new IllegalArgumentException("KNN vector values cannot be NaN");
+            }
+
+            if (Float.isInfinite(value)) {
+                throw new IllegalArgumentException("KNN vector values cannot be infinity");
+            }
+
+            vector.add(value);
+            context.parser().nextToken();
+        } else if (token == XContentParser.Token.VALUE_NULL) {
+            context.path().remove();
+            return;
+        }
+
+        if (dimension != vector.size()) {
+            String errorMessage = String.format("Vector dimension mismatch. Expected: %d, Given: %d", dimension,
+                vector.size());
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        float[] array = new float[vector.size()];
+        int i = 0;
+        for (Float f : vector) {
+            array[i++] = f;
+        }
+
+        KnnVectorField point = new KnnVectorField(name(), array, fieldType);
+
+        context.doc().add(point);
+        context.path().remove();
+    }
+
+    @Override
+    protected boolean docValuesByDefault() {
+        return true;
+    }
+
+    @Override
+    public ParametrizedFieldMapper.Builder getMergeBuilder() {
+        return new KNNVectorFieldMapper.Builder(simpleName()).init(this);
+    }
+
+    @Override
+    public final boolean parsesArrayValue() {
+        return true;
+    }
+
+    @Override
+    public KNNVectorFieldType fieldType() {
+        return (KNNVectorFieldType) super.fieldType();
+    }
+
+    @Override
+    protected void doXContentBody(XContentBuilder builder, boolean includeDefaults, Params params) throws IOException {
+        super.doXContentBody(builder, includeDefaults, params);
+    }
+
+    public static class Names {
+        public static final String IGNORE_MALFORMED = "ignore_malformed";
+    }
+
+    public static class Defaults {
+        public static final Explicit<Boolean> IGNORE_MALFORMED = new Explicit<>(false, false);
+        public static final FieldType FIELD_TYPE = new FieldType();
+
+        static {
+            FIELD_TYPE.setTokenized(false);
+            FIELD_TYPE.setIndexOptions(IndexOptions.NONE);
+            FIELD_TYPE.setDocValuesType(DocValuesType.NONE);
+            FIELD_TYPE.freeze();
+        }
+    }
+}
---
+++ server/src/main/java/org/opensearch/index/query/KNNQueryBuilder.java
@@ -0,0 +1,168 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.index.query;
+
+import org.apache.lucene.search.KnnVectorQuery;
+import org.apache.lucene.search.Query;
+import org.opensearch.common.ParseField;
+import org.opensearch.common.ParsingException;
+import org.opensearch.common.Strings;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.common.io.stream.StreamOutput;
+import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.index.mapper.NumberFieldMapper;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class KNNQueryBuilder extends AbstractQueryBuilder<KNNQueryBuilder> {
+
+    public static final String NAME = "knn";
+
+    public static final ParseField VECTOR_FIELD = new ParseField("vector");
+    public static final ParseField K_FIELD = new ParseField("k");
+
+    private final String fieldName;
+    private final float[] vector;
+    private final int k;
+
+    public KNNQueryBuilder(String fieldName, float[] vector, int k) {
+        if (Strings.isNullOrEmpty(fieldName)) {
+            throw new IllegalArgumentException("[" + NAME + "] requires fieldName");
+        }
+        if (vector == null) {
+            throw new IllegalArgumentException("[" + NAME + "] requires query vector");
+        }
+        if (vector.length == 0) {
+            throw new IllegalArgumentException("[" + NAME + "] query vector is empty");
+        }
+        if (k <= 0) {
+            throw new IllegalArgumentException("[" + NAME + "] requires k > 0");
+        }
+
+        this.fieldName = fieldName;
+        this.vector = vector;
+        this.k = k;
+    }
+
+    /**
+     * @param in Reads from stream
+     * @throws IOException Throws IO Exception
+     */
+    public KNNQueryBuilder(StreamInput in) throws IOException {
+        super(in);
+        try {
+            fieldName = in.readString();
+            vector = in.readFloatArray();
+            k = in.readInt();
+        } catch (IOException ex) {
+            throw new RuntimeException("[KNN] Unable to create KNNQueryBuilder: " + ex);
+        }
+    }
+
+    @Override
+    public String getWriteableName() {
+        return NAME;
+    }
+
+    @Override
+    protected void doWriteTo(StreamOutput out) throws IOException {
+        out.writeString(fieldName);
+        out.writeFloatArray(vector);
+        out.writeInt(k);
+    }
+
+    @Override
+    protected void doXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject(NAME);
+        builder.startObject(fieldName);
+
+        builder.field(VECTOR_FIELD.getPreferredName(), vector);
+        builder.field(K_FIELD.getPreferredName(), k);
+        printBoostAndQueryName(builder);
+        builder.endObject();
+        builder.endObject();
+    }
+
+    @Override
+    protected Query doToQuery(QueryShardContext context) throws IOException {
+        return new KnnVectorQuery(fieldName, vector, k);
+    }
+
+    @Override
+    protected boolean doEquals(KNNQueryBuilder other) {
+        return Objects.equals(fieldName, other.fieldName) &&
+            Arrays.equals(vector, other.vector) &&
+            Objects.equals(k, other.k);
+    }
+
+    @Override
+    protected int doHashCode() {
+        return Objects.hash(fieldName, Arrays.hashCode(vector), k);
+    }
+
+    public static KNNQueryBuilder fromXContent(XContentParser parser) throws IOException {
+        String fieldName = null;
+        List<Object> vector = null;
+        float boost = AbstractQueryBuilder.DEFAULT_BOOST;
+        int k = 0;
+        String queryName = null;
+        String currentFieldName = null;
+        XContentParser.Token token;
+        while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
+            if (token == XContentParser.Token.FIELD_NAME) {
+                currentFieldName = parser.currentName();
+            } else if (token == XContentParser.Token.START_OBJECT) {
+                throwParsingExceptionOnMultipleFields(NAME, parser.getTokenLocation(), fieldName, currentFieldName);
+                fieldName = currentFieldName;
+                while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
+                    if (token == XContentParser.Token.FIELD_NAME) {
+                        currentFieldName = parser.currentName();
+                    } else if (token.isValue() || token == XContentParser.Token.START_ARRAY) {
+                        if (VECTOR_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
+                            vector = parser.list();
+                        } else if (AbstractQueryBuilder.BOOST_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
+                            boost = parser.floatValue();
+                        } else if (K_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
+                            k = (Integer) NumberFieldMapper.NumberType.INTEGER.parse(parser.objectBytes(), false);
+                        } else if (AbstractQueryBuilder.NAME_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
+                            queryName = parser.text();
+                        } else {
+                            throw new ParsingException(parser.getTokenLocation(),
+                                "[" + NAME + "] query does not support [" + currentFieldName + "]");
+                        }
+                    } else {
+                        throw new ParsingException(parser.getTokenLocation(),
+                            "[" + NAME + "] unknown token [" + token + "] after [" + currentFieldName + "]");
+                    }
+                }
+            } else {
+                throwParsingExceptionOnMultipleFields(NAME, parser.getTokenLocation(), fieldName, parser.currentName());
+                fieldName = parser.currentName();
+                vector = parser.list();
+            }
+        }
+
+        KNNQueryBuilder knnQuery = new KNNQueryBuilder(fieldName, ObjectsToFloats(vector), k);
+        knnQuery.queryName(queryName);
+        knnQuery.boost(boost);
+        return knnQuery;
+    }
+
+    private static float[] ObjectsToFloats(List<Object> objs) {
+        float[] vec = new float[objs.size()];
+        for (int i = 0; i < objs.size(); i++) {
+            vec[i] = ((Number) objs.get(i)).floatValue();
+        }
+        return vec;
+    }
+}
